6bd6f96b7674e3172ef099841d2f2e78332f2da8
jgmonroy <jgmonroy@uma.es>
2022-06-23 23:38:49 -0700

    New ROS2 pkg to launch CoppeliaSim as a Node, with parameter to set the simulator path and desired scene to load (with autoplay).

---
+++ coppelia_ros2_pkg/src/run_coppelia_simulator.cpp
@@ -0,0 +1,47 @@
+#include <rclcpp/rclcpp.hpp>
+#include <stdlib.h>
+#include <string>
+
+
+class CoppeliaSim: public rclcpp::Node
+{
+  public:
+    CoppeliaSim()
+    : Node("coppelia_simulator")
+    {
+      //Read parameters and set default values
+      this->declare_parameter<std::string>("coppelia_root_dir", "");
+      this->get_parameter("coppelia_root_dir", coppelia_dir);
+
+      this->declare_parameter<std::string>("coppelia_scene_path", "");
+      this->get_parameter("coppelia_scene_path", coppelia_scene);
+    }
+    
+    void run()
+    {
+      //system("~/CoppeliaSim_Edu_V4_3_0_rev10_Ubuntu20_04/coppeliaSim.sh -s ~/tfg_ros_simulation_ws/src/Coppelia_scenes/ros2_mobile_robot.ttt");
+      system( (coppelia_dir+"/coppeliaSim.sh -s "+ coppelia_scene).c_str() );
+    }
+    
+  private:
+    std::string coppelia_dir, coppelia_scene;
+};
+
+      
+// MAIN
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  
+  //create object
+  std::shared_ptr<CoppeliaSim> myNode = std::make_shared<CoppeliaSim>();
+  myNode->run();
+
+  rclcpp::shutdown();
+  return 0;
+  
+}
+
+
+
+
