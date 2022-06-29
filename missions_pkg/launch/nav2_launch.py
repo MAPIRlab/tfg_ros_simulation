85c490f5dc9420d62e8f7a210e757d684b68aadf
jgmonroy <jgmonroy@uma.es>
2022-06-29 04:39:54 -0700

    Adding Map_Server and AMCL to the simulation with Coppelia. Still some parameter fitting to do.

---
+++ missions_pkg/launch/nav2_launch.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    # Get the launch directory
+    #my_dir = get_package_share_directory('missions_pkg')
+    my_dir = '/home/robotics/tfg_ros_simulation_ws/src/missions_pkg'
+
+    # common variables
+    use_sim_time = True
+    remappings=[]
+    params_yaml_file = os.path.join(my_dir, 'launch', 'nav2_params.yaml')
+    map_file = os.path.join(my_dir, 'maps', 'coppelia_map_gmapping.yaml')
+    
+    return LaunchDescription([
+        # Set env var to print messages to stdout immediately
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'yaml_filename' : map_file}],
+            remappings=remappings),
+
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[params_yaml_file],
+            remappings=remappings),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['map_server','amcl']}])
+    ])
